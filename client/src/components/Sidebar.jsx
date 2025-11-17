5ae72cfc6ffe870528b2ea9f8cec43b07e8982d1
Jeethesh <jeethesh000@gmail.com>
2025-11-17 09:11:38 +0530

    second commit

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAppContext } from '../context/AppContext'
+import { assets } from '../assets/assets';
 
 const Sidebar = () => {
+
+ const {
+  chats,
+  setSelectedChat,
+  theme,
+  setTheme,
+  user
+ } = useAppContext();
+ const [search,setSearch] = useState("")
+
   return (
     <div>
-      
+      <img className='w-full max-w-48'
+       src={theme === "dark" ? assets?.logo_full : assets?.logo_full_dark} alt='' />
     </div>
   )
 }
