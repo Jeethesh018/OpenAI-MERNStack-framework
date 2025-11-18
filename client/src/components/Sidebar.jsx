a9c42a13eb28e7707123891db280421c590a6514
Jeethesh <jeethesh000@gmail.com>
2025-11-18 09:28:49 +0530

    added new chats

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -14,9 +14,40 @@ const Sidebar = () => {
  const [search,setSearch] = useState("")
 
   return (
-    <div>
+    <div className='flrx flex-col h-screen min-w-72 p-5 dark:bg-gradient-to-b from-[#242124]/30 to-[#000000]/30 border-r border-[#80609f]/30  backdrop-blur-3xl transition-all duration-500 max-md:absolute left-0 z-1 '>
       <img className='w-full max-w-48'
        src={theme === "dark" ? assets?.logo_full : assets?.logo_full_dark} alt='' />
+
+       <button className='flex justify-center items-center w-full py-2 mt-10 text-white bg-gradient-to-r from-[#A456F7] to-[#3D81F6] text-sm rounded-md cursor-pointer'>
+        <span className='mr-2 text-xl'>+</span> New Chat
+       </button>
+
+
+       <div className='flex items-center gap-2 p-3 mt-4 border border-gray-400 dark:border-white/20 rounded-md'>
+         <img src={assets.search_icon} className='w-4 not-dark:invert' alt=''/>
+         <input type='text' onChange={(e)=>setSearch(e.target.value)} value={search} placeholder='Search Conversations' className='text-xs placeholder:text-gray-400 outline-none' />
+       </div>
+
+
+       {
+        chats.length>0 && <p className='mt-4 text-sm'>Recent Chats</p>
+       }
+       <div className='flex-1 overflow-y-scroll mt-3 text-sm space-y-3'>
+         {
+          chats.filter((chat)=>chat?.messages[0] ? chat?.messages[0]?.content.toLowerCase().includes(search.toLowerCase()) :
+          chat.name.toLowerCase().includes(search.toLowerCase())).map((chat)=> (
+            <div key={chat._id} className='p-2 px-4 dark:bg-[#57317C]/10 border border-gray-300 dark:border-[#80609F]/15
+            rounded-md cursor-pointer flex justify-between group'>
+               <div>
+                <p className='truncate w-full'>
+                  {chat.messages.length > 0 ? chat.messages[0].content.slice(0,32) : chat.name}
+                </p>
+                <p className='text-sx text-gray-500 dark:text-[#B1A6CO]]'>{chat.updatedAt}</p>
+                </div>
+              </div>
+          ))
+         }
+       </div>
     </div>
   )
 }
