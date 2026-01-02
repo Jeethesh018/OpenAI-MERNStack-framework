73ce327453ecec4db2ca585df25126c16ce8f35a
Jeethesh <jeethesh000@gmail.com>
2026-01-02 20:03:20 +0530

    day3

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -31,7 +31,7 @@ export const AppContextProvider = ({ children }) => {
         else{
             document.documentElement.classList.remove("dark")
         }
-        
+        localStorage.setItem("theme",theme)
   },[theme])
 
   useEffect(() => {
